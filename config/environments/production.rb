e826a8d4cdbe976099ccf1070b63120bde532fb1
Victor Goya <goya.victor@gmail.com>
2019-11-07 11:58:50 +0100

    add cloudfront URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,15 +21,16 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  if ENV['ASSETS_CLOUDFRONT_URL'].present?
+    config.action_controller.asset_host = ENV['ASSETS_CLOUDFRONT_URL']
+  end
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
