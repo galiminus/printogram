0c8e92d939b4bb060d3d866ce112cb90fac8e7a6
Victor Goya <goya.victor@gmail.com>
2019-11-26 20:21:42 +0100

    better looping

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('video').playbackRate = 0.8;
+  var video = document.querySelector('video');
+  var maxLoop = 3;
+  var loop = 0;
+
+  video.onended = function() {
+    loop += 1
+    if (loop == maxLoop) {
+      return;
+    }
+
+    setTimeout(function () {
+      video.currentTime = 0;
+      video.play();
+    }, loop * 2500);
+  }
+  video.play();
+  video.playbackRate = 0.75;
 });
