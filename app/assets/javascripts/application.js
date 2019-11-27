5e1cf82766983d1cffa9cc913f799ac993921c5b
Victor Goya <goya.victor@gmail.com>
2019-11-27 09:07:16 +0100

    add loader

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+  if (window.innerWidth <= 768) {
+    return ;
+  }
   var video = document.querySelector('video');
+  var loading = document.querySelector('#video-loading');
+  var container = document.querySelector("#header-hero-container");
+  var help = document.querySelector("#help-hero-container .hero");
+  var overlay = document.querySelector("#overlay-hero");
+  var ended = false;
+
   var maxLoop = 3;
   var loop = 0;
 
   video.onended = function() {
     loop += 1
     if (loop == maxLoop) {
+      ended = true;
       return;
     }
 
@@ -14,6 +24,41 @@ document.addEventListener('DOMContentLoaded', function() {
       video.play();
     }, loop * 2500);
   }
+  video.onplay = function() {
+    loading.style.display = "none";
+  }
   video.play();
   video.playbackRate = 0.75;
+
+  var lastScroll = 0;
+  var ticking = false;
+  window.addEventListener('scroll', function(e) {
+    lastScroll = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        ticking = false;
+        if (lastScroll > 450) {
+          return;
+        }
+        if (!ended) {
+          if (lastScroll < 50) {
+            video.play();
+          } else {
+            video.pause();
+          }
+        }
+
+        // var newHeight = "calc(78vh - " + lastScroll / 2 + "px)"
+        // container.style.maxHeight = newHeight;
+        // // video.style.height = newHeight;
+        // loading.style.height = newHeight;
+        // overlay.style.height = newHeight;
+
+        // help.style.top = "calc(78vh - 450px - " + lastScroll + "px)";
+      });
+    }
+
+    ticking = true;
+  });
 });
