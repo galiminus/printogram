9191dfdf6435a8eee6fe235d24d08ad5eb6ca468
Victor Goya <goya.victor@gmail.com>
2019-12-02 08:45:59 +0100

    Reduce preview file size

--- app/lib/montage.rb
+++ app/lib/montage.rb
@@ -4,11 +4,11 @@ class Montage
       image_params = order.images.order(created_at: :asc).map(&:download!).map.with_index do |image_path, index|
         "-label #{index + 1} #{image_path.shellescape}"
       end.join(" ")
-      output = "#{wdir}/output.webp"
+      output = "#{wdir}/output.jpeg"
 
-      dimension = [256 / Math.sqrt(order.images.count).floor * 2, 192].max
+      dimension = [128 / Math.sqrt(order.images.count).floor * 2, 96].max
       pointsize = dimension / 8
-      system("montage -quality 70 -font #{Rails.root}/app/assets/fonts/CarterOne-Regular.ttf -pointsize #{pointsize} -geometry #{dimension}x#{dimension}\\>+6+12 -bordercolor white -border 10 #{image_params} #{output}")
+      system("montage -quality 85 -font #{Rails.root}/app/assets/fonts/CarterOne-Regular.ttf -pointsize #{pointsize} -geometry #{dimension}x#{dimension}\\>+6+12 -bordercolor white -border 10 #{image_params} #{output}")
 
       yield output
     end
